e39cc1dd6ac72d94172cf8c718022481e0152069
Caleb Hagner <caleb.hagner@pm.me>
2020-08-06 11:14:30 -0400

    ⚗ Add React Headroom

---
+++ src/components/Menu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Headroom from "react-headroom";
+
+export default function Menu() {
+    return <Headroom>
+        <h1>FRC Jumpstart</h1>
+    </Headroom>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import Menu from "../components/Menu";
 
 export default function Home() {
-  return <div>Hello world!</div>;
+  return <div><Menu /><br />Hello world!</div>;
 }
