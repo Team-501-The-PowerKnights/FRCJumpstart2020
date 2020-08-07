62553cac7ffd5dd91cd7b4ed6224b79775415731
Caleb Hagner <caleb.hagner@pm.me>
2020-08-07 16:36:36 -0400

    ✨ Add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TopMenu from '../components/menu/TopMenu';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Footer from '../components/footer/Footer';
 
 export default function Home() {
   return (
     <>
       <CssBaseline />
       <TopMenu />
+      <Footer />
     </>
   );
 }
