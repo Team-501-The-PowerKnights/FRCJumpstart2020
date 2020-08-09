72be85b28ae4208da591361d53ffb920d3abd0a5
Caleb Hagner <caleb.hagner@pm.me>
2020-08-09 15:12:55 -0400

    ✨ Draft Panel Body

--- src/components/menu/TopMenu.jsx
+++ src/components/menu/TopMenu.jsx
@@ -12,33 +12,40 @@ import logo from '../../images/logo.png';
 
 export default function TopMenu() {
   return (
-    <Headroom>
-      <Navbar style={{
-        backgroundColor: `#2F3292`
-      }}
-        expand='lg' variant='dark'>
-
-        <Navbar.Brand href="/"><img
-          src={logo}
-          alt={`FRC Jumpstart`}
-          height={`100`}
-          style={{ display: `inline` }}
-        /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" color="light" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+    <Headroom style={{ height: `101` }}>
+      <Navbar
+        style={{
+          backgroundColor: `#2F3292`,
+        }}
+        expand='lg'
+        variant='dark'>
+        <Navbar.Brand href='/'>
+          <img src={logo} alt={`FRC Jumpstart`} height={`100`} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
             <MenuItem link='#about' openInNewTab='false' hoverColor='#FFDE17'>
               About
-      </MenuItem>
-            <MenuItem link='#speakers' openInNewTab='false' hoverColor='#FFDE17'>
+            </MenuItem>
+            <MenuItem
+              link='#speakers'
+              openInNewTab='false'
+              hoverColor='#FFDE17'>
               Speakers
-      </MenuItem>
-            <MenuItem link='#schedule' openInNewTab='false' hoverColor='#FFDE17'>
+            </MenuItem>
+            <MenuItem
+              link='#schedule'
+              openInNewTab='false'
+              hoverColor='#FFDE17'>
               Schedule
-      </MenuItem>
-            <MenuItem link='#subscribe' openInNewTab='false' hoverColor='#FFDE17'>
+            </MenuItem>
+            <MenuItem
+              link='#subscribe'
+              openInNewTab='false'
+              hoverColor='#FFDE17'>
               Subscribe
-      </MenuItem>
+            </MenuItem>
           </Nav>
           <SocialMenu hoverColor='#FFDE17' />
         </Navbar.Collapse>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,81 @@ import React from 'react';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import EventIcon from '@material-ui/icons/Event';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
 import TopMenu from '../components/menu/TopMenu';
 import Footer from '../components/footer/Footer';
 
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+
+import banner from '../images/banner.jpg';
+import logo from '../images/logo.png';
+import crowd from '../images/crowd.jpeg';
+
 import 'typeface-nunito-sans';
+import Card from 'react-bootstrap/Card';
+import { ListGroup } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <>
       <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossOrigin="anonymous"
+        rel='stylesheet'
+        href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
+        integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk'
+        crossOrigin='anonymous'
       />
-      <CssBaseline />{' '}
-      {/** Can I modify the CSSBaseline / do theming with my colors and fonts? */}
+      <CssBaseline />
       <TopMenu />
+      <Jumbotron
+        className='text-center'
+        id='about'
+        style={{
+          margin: 0,
+          backgroundImage: `url(${banner})`,
+          backgroundAttachment: `fixed`,
+          backgroundPosition: `center`,
+          backgroundRepeat: `no-repeat`,
+          backgroundSize: `cover`,
+          padding: `17.5%`
+        }}>
+        <Card
+          style={{ width: '18rem', marginLeft: `auto`, marginRight: `auto` }}>
+          <Card.Img variant='top' src={logo} />
+          <Card.Body>
+            <ListGroup variant="flush">
+              <ListGroup.Item><EventIcon /> <p style={{ fontFamily: `Nunito Sans`, display: `inline` }}>November 21, 2020</p></ListGroup.Item>
+              <ListGroup.Item><LocationOnIcon /> <p style={{ fontFamily: `Nunito Sans`, display: `inline` }}>YouTube / Twitch</p></ListGroup.Item>
+            </ListGroup>
+            <Button variant='primary' style={{ backgroundColor: `#2F3292` }}>
+              <a href='/' style={{ color: `#FFFFFF`, textDecoration: `none` }}>
+                Sign Up Now
+              </a>
+            </Button>
+          </Card.Body>
+        </Card>
+      </Jumbotron>
+      <Jumbotron className='text-light' style={{ backgroundColor: `#2F3292`, marginBottom: 0, padding: `10%` }}>
+        <ListGroup style={{ margin: `auto`, maxWidth: `45rem`, }} variant='flush'>
+          <ListGroup.Item className='text-light' style={{ backgroundColor: `#2F3292` }}><h1>FRC Jumpstart</h1></ListGroup.Item>
+          <ListGroup.Item className='text-light' style={{ backgroundColor: `#2F3292` }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisi ex, euismod dignissim sem at, aliquet congue felis. Duis scelerisque luctus lobortis. Cras tempor fringilla dictum. Vivamus at urna odio. Cras id dignissim mi. Duis luctus, libero vitae elementum tincidunt, dolor leo efficitur sem, sed vehicula diam sapien at nibh. Aliquam pharetra nulla vel vehicula sodales. Phasellus ornare, nisl in vulputate eleifend, libero ipsum vulputate ex, eget vehicula nisl nisl eu nisl. </ListGroup.Item>
+        </ListGroup>
+      </Jumbotron>
+      <Jumbotron
+        className='text-center'
+        style={{
+          margin: 0,
+          backgroundImage: `url(${crowd})`,
+          backgroundAttachment: `fixed`,
+          backgroundPosition: `center`,
+          backgroundRepeat: `no-repeat`,
+          backgroundSize: `cover`,
+          padding: `15%`
+        }}>
+      </Jumbotron>
       <Footer />
     </>
   );
